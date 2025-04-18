a3ff899644409fa821fa248895fe553a3d7d66f5
derussell23 <djamesr23@gmail.com>
2025-04-18 00:45:02 -0700

    Initial commit of Connect Four game

---
+++ build/CMakeFiles/connect4.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/connect4.dir/src/cpp/Game.cpp.o"
+  "CMakeFiles/connect4.dir/src/cpp/Game.cpp.o.d"
+  "CMakeFiles/connect4.dir/src/cpp/main.cpp.o"
+  "CMakeFiles/connect4.dir/src/cpp/main.cpp.o.d"
+  "connect4"
+  "connect4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/connect4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ c_cpp/src/cpp/Game.hpp
@@ -0,0 +1,7 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#include <vector>
+#include <iostream>
+
+#endif // GAME_HPP
